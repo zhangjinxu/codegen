22e7485278d427a35bc27f4e603230021e1d1821
zhangjinxu <zhangjinxu0211@163.com>
2018-12-08 00:20:25 +0800

    mybatis generator初步组织

--- code-generator/src/main/java/com/zjx/opensource/codegenerator/CodeGenerator.java
+++ code-generator/src/main/java/com/zjx/opensource/codegenerator/CodeGenerator.java
@@ -1,4 +1,10 @@
 package com.zjx.opensource.codegenerator;
 
+import com.zjx.opensource.codegenerator.mbg.MBGGenerator;
+
 public class CodeGenerator {
+
+    public static void main(String[] args) throws Exception {
+        MBGGenerator.generate();
+    }
 }
---
+++ code-generator/src/main/java/com/zjx/opensource/codegenerator/mbg/CodegenCommentGenerator.java
@@ -0,0 +1,8 @@
+package com.zjx.opensource.codegenerator.mbg;
+
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+public class CodegenCommentGenerator extends DefaultCommentGenerator {
+
+
+}
