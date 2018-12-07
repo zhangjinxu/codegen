4ac5bd582daa983f93f5615e8dfdc41f4bf25e02
zhangjinxu <zhangjinxu0211@163.com>
2018-12-07 21:52:11 +0800

    初次提交

---
+++ src/main/java/com/zjx/opensource/codegen/CodegenApplication.java
@@ -0,0 +1,12 @@
+package com.zjx.opensource.codegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodegenApplication.class, args);
+    }
+}
